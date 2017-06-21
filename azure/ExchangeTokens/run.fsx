901862b813de73621756d7a3c42e42cde5b0e3f1
Joe Fiorini <joe@joefiorini.com>
2017-06-21 18:17:18 -0400

    New deployment setup

--- azure/ExchangeTokens/run.fsx
+++ azure/ExchangeTokens/run.fsx
@@ -1,29 +1,50 @@
 #r "System.Net.Http"
 #r "Newtonsoft.Json"
-#r "MoneyAlarms.dll"
+#r "MoneyAlarms.Core.dll"
 
 #load ".paket/load/fsharp.data.fsx"
 
+open System
 open System.Net
 open System.Net.Http
 open Newtonsoft.Json
-open MoneyAlarms
+open MoneyAlarms.Core
 open FSharp.Data
+
+type Result<'Ok,'Error> =
+    | Ok of 'Ok
+    | Error of 'Error
 
 let Run(req: HttpRequestMessage, log: TraceWriter) =
     async {
         let! data = req.Content.ReadAsStringAsync() |> Async.AwaitTask
 
-        if (String.isNullOrEmpty(data)) then
-          return req.CreateResponse(HttpStatusCode.BadRequest, "Need a body")
-        else
-          let dto = JsonConvert.DeserializeObject<TokenExchangeDto>(data)
-          let plaidExchangeToken' = Plaid.configurePlaidService "" "" "" Plaid.plaidExchangeToken
-          let! account = (createAccount plaidExchangeToken' dto) |> Async.AwaitTask
+        // if (String.IsNullOrEmpty(data)) then
+        //     return req.CreateResponse(HttpStatusCode.BadRequest, "Need a body")
+        // else
+        let httpClient = new HttpClient()
+        let dto = JsonConvert.DeserializeObject<TokenExchangeDto>(data)
 
+        let plaidConfig = Plaid.ServiceConfig.fromEnvironment(httpClient)
+        let plaidExchangeToken = Plaid.plaidExchangeToken plaidConfig
+
+        let firebaseToken = Firebase.AccessToken.generate()
+        let firebaseConfig =
+                Firebase.ServiceConfig.fromEnvironment httpClient firebaseToken
+
+        let firebaseCreateAccount =
+              Firebase.SaveAccount.firebaseCreateAccount firebaseConfig
+
+        let exchangeResult =
+            ExchangeTokens.createAccount
+              plaidExchangeToken
+              firebaseCreateAccount
+              dto
+
+        return
           match exchangeResult with
-          | Ok () ->
-              return req.CreateResponse(HttpStatusCode.OK, "Good");
+          | Ok _ ->
+              req.CreateResponse(HttpStatusCode.OK, "Good");
           | Error e ->
-              return req.CreateResponse(HttpStatusCode.InternalServerError, sprintf "Error: %s" e)
-    } |> Async.RunSynchronously
+              req.CreateResponse(HttpStatusCode.InternalServerError, sprintf "Error: %s" e)
+    } |> Async.StartAsTask
